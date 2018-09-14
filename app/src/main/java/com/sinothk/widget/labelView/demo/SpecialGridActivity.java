d9a4bbc4afa51c432947922a312e044f448b4a7e
LiangYT <381518188@qq.com>
2018-09-14 11:46:39 +0800

    新增View

--- app/src/main/java/com/sinothk/widget/labelView/demo/SpecialGridActivity.java
+++ app/src/main/java/com/sinothk/widget/labelView/demo/SpecialGridActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.RadioGroup;
 
 import com.sinothk.widget.labelView.style1.AutoFlowLayout;
 import com.sinothk.widget.labelView.style1.FlowAdapter;
@@ -25,10 +27,18 @@ public class SpecialGridActivity extends AppCompatActivity {
         setContentView(R.layout.special_grid);
 
         mFlowLayout = (AutoFlowLayout) findViewById(R.id.afl_cotent);
+        mFlowLayout.setRowNumbers(3);// 行
+        mFlowLayout.setColumnNumbers(3); // 列
         mFlowLayout.setAdapter(new FlowAdapter(Arrays.asList(mData)) {
             @Override
             public View getView(int position) {
+//                WindowManager wm = SpecialGridActivity.this.getWindowManager();
+//                int width = wm.getDefaultDisplay().getWidth();
+//                int height = wm.getDefaultDisplay().getHeight();
+
                 View item = LayoutInflater.from(SpecialGridActivity.this).inflate(R.layout.special_grid_item, null);
+//                item.setLayoutParams(new RadioGroup.LayoutParams(width/3,width/3));
+
                 return item;
             }
         });
